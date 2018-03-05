d7175737ed4533a909c7055f9bb79d9d54b733ed
everpcpc <git@everpcpc.com>
2018-03-05 17:06:46 +0800

    Split files

--- app.go
+++ app.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"context"
-	"net/http"
 	"net/url"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
@@ -43,39 +41,8 @@ func main() {
 
 	auth := r.Group("/oauth")
 	{
-		auth.GET("/callback", authCallback)
-		auth.GET("/url", authURL)
+		auth.GET("/callback", oauthCallback)
+		auth.GET("/url", oauthURL)
+		r.Run(viper.GetString("listen"))
 	}
-
-	r.Run(viper.GetString("listen"))
-}
-
-func authURL(c *gin.Context) {
-	url := oauthClient.AuthCodeURL("", oauth2.AccessTypeOnline)
-	c.JSON(200, gin.H{
-		"url": url,
-	})
-}
-
-func authCallback(c *gin.Context) {
-	code := c.Query("code")
-	proxyedClient := &http.Client{
-		Timeout:   10 * time.Second,
-		Transport: &http.Transport{Proxy: http.ProxyURL(proxyURL)},
-	}
-	ctx = context.WithValue(ctx, oauth2.HTTPClient, proxyedClient)
-	token, err := oauthClient.Exchange(ctx, code)
-	if err != nil {
-		c.JSON(200, gin.H{
-			"ok":      false,
-			"err_msg": err.Error(),
-		})
-		return
-	}
-
-	c.JSON(200, gin.H{
-		"OK":    true,
-		"code":  code,
-		"token": token,
-	})
 }
---
+++ oauth.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+)
+
+func oauthURL(c *gin.Context) {
+	url := oauthClient.AuthCodeURL("", oauth2.AccessTypeOnline)
+	c.JSON(200, gin.H{
+		"url": url,
+	})
+}
+
+func oauthCallback(c *gin.Context) {
+	code := c.Query("code")
+	proxyedClient := &http.Client{
+		Timeout:   10 * time.Second,
+		Transport: &http.Transport{Proxy: http.ProxyURL(proxyURL)},
+	}
+	ctx = context.WithValue(ctx, oauth2.HTTPClient, proxyedClient)
+	token, err := oauthClient.Exchange(ctx, code)
+	if err != nil {
+		c.JSON(200, gin.H{
+			"ok":      false,
+			"err_msg": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"OK":    true,
+		"code":  code,
+		"token": token,
+	})
+}
